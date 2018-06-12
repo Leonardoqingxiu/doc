2908f6bf799a90eee9800d3b5243af7bd609699e
feng <745392252@qq.com>
2018-06-12 10:45:54 +0800

    66

--- linux常用.php
+++ linux常用.php
@@ -1,7 +1,14 @@
 linux下配置：
-git
+git编译安装
 https://www.cnblogs.com/lonecloud/p/7399804.html
 
+git yum安装
+yum install -y git
 
-linux撤销行
-u
+u linux撤销行
+
+rm -f 文件名 删除文件命令
+rm -rf  目录名字
+其中：
+-r：向下递归删除
+-f：直接强行删除，且没有任何提示
